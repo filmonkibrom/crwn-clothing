8f9047ee7371d4f4976d7e1112dc2f1450971992
filmon Kibrom <kibromfilmon@gmail.com>
2020-06-04 00:04:44 -0500

    components are added like custom-button,form-input,sign-in-and-sign-up, and sign-in page are added.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import Shop_Data from './pages/shops/shop.components';
 import Header from './components/header/header.component';
+import SingInAndSignUpPages from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 
 
 
@@ -14,6 +16,7 @@ function App() {
        <Switch>
        <Route exact path='/' component={HomePage} />
        <Route path='/Shop' component ={Shop_Data}/>
+       <Route path='/signin' component ={SingInAndSignUpPages}/>
        </Switch>
        
      </div>
