b03ac2bfcdf36d2f2f71e1bb728f583807f85f5d
filmon Kibrom <kibromfilmon@gmail.com>
2020-06-11 22:31:54 -0500

    implemented firebase utils,includs ability to store authenticated users in to our firestore database

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './sign-in.styles.scss';
-import FormInput from '../../components/form-input/form-input.component';
-import CustomButton from '../../components/custom-button/custom-button.component';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component{
